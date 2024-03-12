4b9a17197b94448c95b1147ce6b09f154acd0580
Harivan-T <harivantahir@gmail.com>
2024-03-12 15:05:43 +0300

    completed

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(num1,num2){
+    return num1+num2;
+}
+function subtract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
+}
+function increment(n){
+    return ++n;
+}
+function decrement(n){
+    return --n;
+}
+function makeInt(n){
+     return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+    return parseFloat(n);
+  }
